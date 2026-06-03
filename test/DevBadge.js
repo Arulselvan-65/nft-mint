a2fef4013213f06f9add70436f40aadc76bb525c
Arulselvan-65 <aruslelvan2244@gmail.com>
2026-06-03 21:40:21 +0530

    Added test file

--- test/DevBadge.js
+++ test/DevBadge.js
@@ -5,24 +5,12 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
+describe("DevBadge", function () {
+  async function deployDevBadgeFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    const DevBadge = await ethers.getContractFactory("DevBadge");
+    const contract = await DevBadge.deploy();
+    return { contract, owner, otherAccount };
   }
 
   describe("Deployment", function () {
