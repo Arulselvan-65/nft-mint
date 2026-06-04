6d2366ea3addbc656c247834ecc5152b264463a7
Arulselvan-65 <arulselvan2244@gmail.com>
2026-06-04 16:05:00 +0530

    Updated contract and added testcases for Deployment, Mint and Withdraw functions

--- contracts/DevBadge.sol
+++ contracts/DevBadge.sol
@@ -3,57 +3,53 @@ pragma solidity ^0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {
+    ERC721URIStorage
+} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract DevBadge is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
-    uint256 public constant MINT_PRICE = 0.001 ether; 
-    int8 public constant MAX_SUPPLY = 100; 
+    uint256 public constant MINT_PRICE = 0.001 ether;
+    uint256 public constant MAX_SUPPLY = 100;
+    string public BASE_URI;
 
     mapping(address => bool) public hasMinted;
 
-    constructor()
-        ERC721("DevBadge", "DBDG")
-        Ownable(msg.sender)
-    {}
+    constructor(
+        string memory baseURI
+    ) ERC721("DevBadge", "DBDG") Ownable(msg.sender) {
+        BASE_URI = baseURI;
+    }
 
     error ExceedsMintLimit();
     error AlreadyMinted();
     error InsufficientPayment(uint256 required, uint256 provided);
+    error TransferFailed();
 
     event TokenMinted(address indexed to, uint256 tokenId);
 
-    function safeMint(address to, string memory uri) external payable {
+    function safeMint() external payable {
         uint256 tokenId = _nextTokenId++;
-        require(msg.value == MINT_PRICE, InsufficientPayment(MINT_PRICE, msg.value));
-        require(!hasMinted[msg.sender], AlreadyMinted());
-        if(tokenId > 100) revert ExceedsMintLimit();
+        if (tokenId >= MAX_SUPPLY) revert ExceedsMintLimit();
+        if (hasMinted[msg.sender]) revert AlreadyMinted();
+        if (msg.value != MINT_PRICE)
+            revert InsufficientPayment(MINT_PRICE, msg.value);
         hasMinted[msg.sender] = true;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        emit TokenMinted(to, tokenId);
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, BASE_URI);
+        emit TokenMinted(msg.sender, tokenId);
     }
 
     function withdraw() external onlyOwner {
         (bool s, ) = msg.sender.call{value: address(this).balance}("");
-        require(s, "Transfer Failed");
+        require(s, TransferFailed());
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
--- test/DevBadge.js
+++ test/DevBadge.js
@@ -4,111 +4,102 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("DevBadge", function () {
   async function deployDevBadgeFixture() {
+    const tokenURI = "https://ipfs.io/ipfs/bafkreibckfqyifkptko6azgni6ioaxrlbdmc5koa4q2g4qffec523qykyi";
     const [owner, otherAccount] = await ethers.getSigners();
     const DevBadge = await ethers.getContractFactory("DevBadge");
-    const contract = await DevBadge.deploy();
+    const contract = await DevBadge.deploy(tokenURI);
     return { contract, owner, otherAccount };
   }
 
   describe("Deployment", function () {
-    it("Should set the right owner", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+    it("Should set the right token name", async function () {
+      const { contract } = await loadFixture(deployDevBadgeFixture);
+      expect(await contract.name()).to.equal("DevBadge");
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
+    it("Should set the right token symbol", async function () {
+      const { contract } = await loadFixture(deployDevBadgeFixture);
+      expect(await contract.symbol()).to.equal("DBDG");
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture,
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount,
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future",
-      );
+    it("Should set the correct owner", async function () {
+      const { contract, owner } = await loadFixture(deployDevBadgeFixture);
+      expect(await contract.owner()).to.equal(owner);
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet",
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner",
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+  describe("Mint", function () {
+    it("Should allow everyone to mint NFT", async function () {
+      const { contract, owner } = await loadFixture(deployDevBadgeFixture);
+      await contract.safeMint({ value: await contract.MINT_PRICE() });
+      expect(await contract.balanceOf(owner.address)).to.equal(1);
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+    it("Should assign the correct token ID to the minted NFT", async function () {
+      const { contract, owner } = await loadFixture(deployDevBadgeFixture);
+      await contract.safeMint({ value: await contract.MINT_PRICE() });
+      expect(await contract.ownerOf(0)).to.equal(owner.address);
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture,
-        );
+    it("Should throw InsufficientPayment if the user does not send enough ETH for minting.", async function () {
+      const { contract, owner } = await loadFixture(deployDevBadgeFixture);
+      expect(contract.safeMint())
+        .to
+        .revertedWithCustomError(contract, "InsufficientPayment")
+        .withArgs(ethers.parseEther("0.001"), 0);
+    });
 
-        await time.increaseTo(unlockTime);
+    it("Should throw AlreadyMinted if the user already minted the NFT", async function () {
+      const { contract, owner } = await loadFixture(deployDevBadgeFixture);
+      await contract.safeMint({ value: await contract.MINT_PRICE() });
+      expect(contract.safeMint())
+        .to
+        .revertedWithCustomError(contract, "AlreadyMinted");
+    });
 
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount],
-        );
-      });
+    it("Should throw ExceedsMintLimit when the maximum supply is reached", async function () {
+      const { contract, owner } = await loadFixture(deployDevBadgeFixture);
+      for (var i = 0; i <= 99; i++) {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+
+        await owner.sendTransaction({
+          to: wallet.address,
+          value: ethers.parseEther("1"),
+        });
+
+        await contract.connect(wallet).safeMint({ value: await contract.MINT_PRICE() });
+      }
+      expect(contract.safeMint())
+        .to
+        .revertedWithCustomError(contract, "ExceedsMintLimit");
+    });
+  });
+
+  describe("Withdraw", function () {
+    it("Should allow the owner to withdraw the contract balance", async function () {
+      const { contract } = await loadFixture(deployDevBadgeFixture);
+      await contract.safeMint({ value: await contract.MINT_PRICE() });
+      expect(await contract.withdraw()).to.changeEtherBalance(0);
+    });
+
+    it("Should allow owner to withdraw all the contract balance", async function () {
+      const { contract } = await loadFixture(deployDevBadgeFixture);
+      await contract.safeMint({ value: await contract.MINT_PRICE() });
+      await contract.withdraw()
+      expect(await ethers.provider.getBalance(contract.target)).to.equal(0);
+    });
+
+    it("Should throw OwnableUnauthorizedAccount when users try to withdraw", async function () {
+      const { contract, otherAccount } = await loadFixture(deployDevBadgeFixture);
+      await contract.safeMint({ value: await contract.MINT_PRICE() });
+      expect(contract.connect(otherAccount).withdraw())
+        .to
+        .revertedWithCustomError(contract, "OwnableUnauthorizedAccount")
+        .withArgs(otherAccount.address);
     });
   });
 });
